0ac4dcbddad87e4e5c3602ed09010dcfd656f8db
imajinyun <imajinyun@gmail.com>
2017-07-23 13:03:59 +0800

    : ) Update StringKit & StringKit test

--- src/Utility/StringKit.php
+++ src/Utility/StringKit.php
@@ -10,7 +10,7 @@ namespace Toolkit\Utility;
 class StringKit
 {
     /**
-     * Get chinese string length.
+     * Get string length.
      *
      * @param string $string
      * @return int
@@ -18,5 +18,22 @@ class StringKit
     public static function length(string $string) : int
     {
         return mb_strlen($string, 'UTF-8');
+    }
+
+    /**
+     * Get part of string.
+     *
+     * @param string   $string
+     * @param int      $start
+     * @param int|null $length
+     * @return string
+     */
+    public static function substr(string $string, int $start, int $length = null) : string
+    {
+        if ($length === null) {
+            $length = self::length($string);
+        }
+
+        return mb_substr($string, $start, $length, 'UTF-8');
     }
 }
--- tests/Utility/StringKitTest.php
+++ tests/Utility/StringKitTest.php
@@ -7,10 +7,24 @@ use Toolkit\Utility\StringKit;
 
 class StringKitTest extends TestCase
 {
-    public function testStringLength()
-    {
-        $string = '我是中国人, 我来自伟大的中华人民共和国';
+    private $string;
 
-        self::assertEquals(20, StringKit::length($string));
+    protected function setUp()
+    {
+        $this->string = '我是中国人, 我来自伟大的中华人民共和国';
+    }
+
+    public function testLengthString()
+    {
+        $actual = StringKit::length($this->string);
+        self::assertEquals(20, $actual);
+    }
+
+    public function testSubString()
+    {
+        $expect = '我是中国人, 我来自';
+        $actual = StringKit::substr($this->string, 0, 10);
+
+        self::assertEquals($expect, $actual);
     }
 }
